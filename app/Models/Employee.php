1f1ab11dd0327692cc3eac21992642d5c1ffedc5
ngendakuriyoleonce <ndendakuriyoleonce75@gmail.com>
2026-06-29 18:02:20 +0400

    Implement Employee Model

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
-    //
+    protected $fillable = [
+
+        'first_name',
+        'last_name',
+        'email',
+        'phone',
+        'position',
+        'salary',
+        'hire_date'
+
+    ];
 }

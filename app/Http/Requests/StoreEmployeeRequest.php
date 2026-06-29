8a51058478345daad50db5fdc06df0048bec1059
ngendakuriyoleonce <ndendakuriyoleonce75@gmail.com>
2026-06-29 18:11:48 +0400

    implement employee validation rules

---
+++ app/Http/Requests/StoreEmployeeRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreEmployeeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+             'first_name'=>'required|string|max:255',
+
+            'last_name'=>'required|string|max:255',
+
+            'email'=>'required|email|unique:employees,email',
+
+            'phone'=>'nullable|string',
+
+            'position'=>'required|string',
+
+            'salary'=>'required|numeric',
+
+            'hire_date'=>'required|date'
+        ];
+    }
+}
